d1cb11bc1636b83f62e1ea178e3d01fe5e2a060f
webGhoul <84246173+web-ghoul@users.noreply.github.com>
2023-09-12 04:04:48 +0300

    added Functions

---
+++ FUNCTIONS/reactToastify.js
@@ -0,0 +1,49 @@
+import { toast } from "react-toastify";
+
+export const handleAlertToastify = (msg, type) => {
+  if (type === "info") {
+    toast.info(msg, {
+      position: "top-center",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+    });
+  } else if (type === "error") {
+    toast.error(msg, {
+      position: "top-left",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+    });
+  } else if(type==="success"){
+    toast.success(msg, {
+      position: "top-right",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+    });
+  }else{
+    toast.warning(msg, {
+      position: "bottom-center",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+    });
+  }
+};
